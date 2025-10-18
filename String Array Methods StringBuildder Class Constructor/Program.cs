ed4f837c1dee491bd290e317207bbbae694c3a86
Farkhad9 <farhadai@code.edu.az>
2025-10-19 00:59:09 +0400

    added task 5

---
+++ String Array Methods StringBuildder Class Constructor/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Text;
+
+namespace String_Array_Methods_StringBuildder_Class_Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task5
+            //string word = "Azerbaijan!";
+            //Console.WriteLine(ReverseStr(word)); 
+            #endregion
+        }
+
+        #region task5
+        private static string ReverseStr(string word)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = word.Length-1; i >= 0; i--)
+            { 
+            stringBuilder.Append(word[i]);
+            }
+            return stringBuilder.ToString();
+        }
+        #endregion
+    }
+}
